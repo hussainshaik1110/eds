21c391bd02ed92aee405dabcae9d04011c3b9d70
Shaik Hussain <stdy2k24@gmail.com>
2025-05-15 16:35:59 +0530

    weather js

---
+++ blocks/weather/weather.js
@@ -0,0 +1,24 @@
+export default async function decorate(block) {
+    const apiUrl = 'https://api.weatherapi.com/v1/current.json?key=demo&q=London';
+  
+    // Try using browser Cache API
+    const cacheKey = new Request(apiUrl);
+    const cache = await caches.open('weather-block-cache');
+  
+    let response = await cache.match(cacheKey);
+  
+    if (!response) {
+      try {
+        response = await fetch(apiUrl);
+        if (response.ok) {
+          await cache.put(cacheKey, response.clone());
+        } else {
+          block.innerHTML = `<p>Error fetching weather data</p>`;
+          return;
+        }
+      } catch (e) {
+        block.innerHTML = `<p>Network error</p>`;
+        return;
+      }
+    }
+}
