28188e85d49c5b18d843ac520edbdc18afa06b93
hussainshaik1110 <stdy2k24@gmail.com>
2025-05-20 19:34:11 +0530

    Update footer.js

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -5,16 +5,22 @@ import { loadFragment } from '../fragment/fragment.js';
  * loads and decorates the footer
  * @param {Element} block The footer block element
  */
+
+console.log("My script loaded1 ✅");
 export default async function decorate(block) {
   // load footer as fragment
   const footerMeta = getMetadata('footer');
   const footerPath = footerMeta ? new URL(footerMeta, window.location).pathname : '/footer';
   const fragment = await loadFragment(footerPath);
+console.log("My script loaded2 ✅");
 
   // decorate footer DOM
   block.textContent = '';
   const footer = document.createElement('div');
   while (fragment.firstElementChild) footer.append(fragment.firstElementChild);
+console.log("My script loaded3 ✅");
 
   block.append(footer);
+  console.log("My script loaded4 ✅");
+
 }
