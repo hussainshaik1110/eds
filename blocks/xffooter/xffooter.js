a73c4e684643a31f33bc436aaa59cf3fb3f76e3c
hussainshaik1110 <stdy2k24@gmail.com>
2025-05-20 19:43:28 +0530

    Update xffooter.js

--- blocks/xffooter/xffooter.js
+++ blocks/xffooter/xffooter.js
@@ -1,16 +1,20 @@
 import { getMetadata } from '../../scripts/aem.js';
+console.log("My script loaded1 ✅");
 
 document.addEventListener('DOMContentLoaded', () => {
   // Select all <li> elements with a nested <ul>
   const dropdownItems = document.querySelectorAll('li > ul');
+console.log("My script loaded2 ✅");
 
   dropdownItems.forEach(submenu => {
     const parent = submenu.parentElement;
     parent.classList.add('dropdown');
+console.log("My script loaded3 ✅");
 
     // Add a toggle icon or just click on the parent text
     parent.addEventListener('click', () => {
       parent.classList.toggle('open');
+      console.log("My script loaded4 ✅");
     });
   });
 });
